dfbcaa29ce771a2b00790a00598789610d65b2dd
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-09 17:07:02 +0430

    entities and date classes created

---
+++ src/main/java/entities/Book.java
@@ -0,0 +1,36 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Book extends Entity{
+    private long isbn;
+    private int onLoan;     // 0 if nobody loan this book, or student id if someone loan it
+    private String name;
+    private String author;
+    private String publisher;
+    private final String constFileName = "Book";
+
+    public Book() {
+        this.entityFileName = constFileName;
+//        this.adaptor.setFileName(constFileName+".txt");
+    }
+
+    public Book(long isbn, String name, String author, String publisher) {
+        this.isbn = isbn;
+        this.onLoan = 0;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+
+        this.entityFileName = constFileName;
+//        this.adaptor.setFileName(constFileName+".txt");
+    }
+
+    public void create() {}
+    public ArrayList<Integer> find(int option) {
+        return new ArrayList<Integer>();
+    }
+    public void get(int index) {}
+    public void edit(int option, int index) {}
+    public void delete(int index) {}
+}
---
+++ src/main/java/entities/Entity.java
@@ -0,0 +1,29 @@
+package entities;
+
+import org.omg.CORBA.INTERNAL;
+
+import java.util.ArrayList;
+
+public abstract class Entity {
+    protected int uniqueId;
+    protected String entityFileName;
+    protected ArrayList<String> fieldsName;
+
+    public String getEntityFileName() {
+        return entityFileName;
+    }
+    public int getUniqueId() {
+        return uniqueId;
+    }
+    public ArrayList<String> getFieldsName() {
+        return fieldsName;
+    }
+//    int objectCount();
+//    public abstract void printAllObjects(); //check if it can implement in Entity
+//    virtual bool checkConfigValidation(Config &config) = 0;
+    public abstract void create();
+    public abstract ArrayList<Integer> find(int option);
+    public abstract void get(int index);
+    public abstract void edit(int option, int index);
+    public abstract void delete(int index);
+}
