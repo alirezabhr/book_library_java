eaedf89bdceb316ce97eb3ac99f8f5fac164c49e
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-11 00:53:45 +0430

    objectCount method implemented for Entity class

--- src/main/java/model/entities/Entity.java
+++ src/main/java/model/entities/Entity.java
@@ -2,13 +2,17 @@ package model.entities;
 
 import controller.adaptors.Adaptor;
 import controller.configs.BaseConfig;
+import controller.file_stream.AppendableObjectOutputStream;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public abstract class Entity {
     protected Adaptor adaptor;
     protected BaseConfig baseConfig;
     protected int uniqueId;
+    protected final String constBaseFilePath = "./database/";
     protected String entityFileName;
     protected ArrayList<String> fieldsName;
 
@@ -26,12 +30,20 @@ public abstract class Entity {
     // abstract methods
 //    public abstract void printAllObjects(); //check if it can implement in Entity
     public abstract void create() throws Exception;
-    public abstract boolean checkConfigValidation();
     public abstract ArrayList<Integer> find(final int option);
-    public abstract void get(final int index);
+    public abstract void get(final int index) throws IndexOutOfBoundsException;
     public abstract void edit(final int option, final int index);
     public abstract void delete(final int index);
 
     // methods
-    //    int objectCount();
+    int objectCount() {
+        int count = 0;
+        try {
+            while (true){
+                this.get(count++);
+            }
+        } catch (IndexOutOfBoundsException exception) {
+            return count-1;
+        }
+    }
 }
