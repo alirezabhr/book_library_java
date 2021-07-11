50a832e4d66b8b828d715542ee1c68db072d4b7f
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-12 01:30:31 +0430

    getMainScene method changed. Tables are correct but displayed data are not correct in BookView and RecordView

--- src/main/java/views/MainPage.java
+++ src/main/java/views/MainPage.java
@@ -3,6 +3,8 @@ package views;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import views.entities_view.BookView;
+import views.entities_view.RecordView;
 import views.entities_view.StudentView;
 
 public class MainPage {
@@ -10,8 +12,8 @@ public class MainPage {
         TabPane tabPane = new TabPane();
 
         Tab studentTab = StudentView.getTab();
-        Tab bookTab = new Tab("Books"  , new Label("Show all books"));
-        Tab recordTab = new Tab("Records" , new Label("Show all records"));
+        Tab bookTab = BookView.getTab();
+        Tab recordTab = RecordView.getTab();
 
         tabPane.getTabs().add(studentTab);
         tabPane.getTabs().add(bookTab);
