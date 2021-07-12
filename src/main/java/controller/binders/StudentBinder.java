0dcf436a412d896e783f5314b8ce3980d3ddd408
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-12 16:51:48 +0430

    some method added in StudentBinder to get Student and checkStudentValidation

--- src/main/java/controller/binders/StudentBinder.java
+++ src/main/java/controller/binders/StudentBinder.java
@@ -1,6 +1,7 @@
 package controller.binders;
 
 import controller.adaptors.Adaptor;
+import controller.configs.BaseConfig;
 import controller.configs.StudentConfig;
 import controller.utils;
 import model.entities.Entity;
@@ -9,9 +10,14 @@ import constant.MyConst;
 
 import java.util.ArrayList;
 
+import static controller.utils.isIntNumber;
+
 public class StudentBinder {
+    private static BaseConfig getConfig() {
+        return new StudentConfig(MyConst.constStudentConfigFilePathName);
+    }
     private static Student createTmpObject() {
-        StudentConfig config = new StudentConfig(MyConst.constStudentConfigFilePathName);
+        StudentConfig config = (StudentConfig) getConfig();
         Adaptor adaptor = utils.getAdaptor(config);
         return new Student(adaptor, config);
     }
@@ -25,5 +31,35 @@ public class StudentBinder {
         }
 
         return students;
+    }
+    public static String checkStudentValidation(String... fields) {
+        String name = fields[0];
+        String lastName = fields[1];
+        String stdIdString = fields[2];
+        int stdId;
+
+        if (!isIntNumber(stdIdString)) {
+            return "Student Id Must Be Integer";
+        }
+        stdId = Integer.parseInt(stdIdString);
+
+        StudentConfig config = (StudentConfig) getConfig();
+        Adaptor adaptor = utils.getAdaptor(config);
+        Student student = new Student(adaptor, config, stdId, name, lastName);
+
+        if (!adaptor.isValidObject(student)) {
+            return "Not A Valid Student With This Student Config!";
+        }
+
+        return "valid";
+    }
+    public static Student getStudent(Object... objects) {
+        String name = (String) objects[0];
+        String lastName = (String) objects[1];
+        Integer stdIdString = (Integer) objects[2];
+
+        StudentConfig config = (StudentConfig) getConfig();
+        Adaptor adaptor = utils.getAdaptor(config);
+        return new Student(adaptor, config, stdIdString, name, lastName);
     }
 }
