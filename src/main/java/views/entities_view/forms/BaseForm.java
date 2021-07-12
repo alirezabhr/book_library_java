760899611f639f97fa3808e43d378294805d2ed7
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-12 16:50:27 +0430

    BaseForm methods added

--- src/main/java/views/entities_view/forms/BaseForm.java
+++ src/main/java/views/entities_view/forms/BaseForm.java
@@ -1,4 +1,31 @@
 package views.entities_view.forms;
 
-public class BaseForm {
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import constant.MyConst;
+
+public abstract class BaseForm {
+
+    // abstract methods
+    public abstract VBox createFormDetail();
+    public abstract VBox editFormDetail();
+    public abstract VBox deleteFormDetail();
+    protected abstract void createObject(Object... objects);
+    protected abstract void editObject();
+    protected abstract void deleteObject();
+
+    // methods
+    public void showForm(String formTitle, VBox root) {
+        Scene scene = new Scene(root);
+        Stage formStage = new Stage();
+        formStage.setScene(scene);
+        formStage.setTitle(formTitle);
+        formStage.setHeight(450);
+        formStage.setWidth(320);
+        formStage.getIcons().add(new Image(MyConst.constAppIconPathName));
+        formStage.show();
+    }
 }
