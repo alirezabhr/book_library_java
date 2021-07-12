d992d5299453434f7ad882c3f58d57738e86771a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-12 13:19:45 +0430

    MyConst class created to retain constant path and name of files

---
+++ src/main/java/constant/MyConst.java
@@ -0,0 +1,22 @@
+package constant;
+
+public class MyConst {
+    private static final String constConfigsPath = "./configs/";
+    private static final String constDatabasesPath = "./database/";
+    private static final String constImagesPath = "./images/";
+
+
+    public static final String constStudentConfigFilePathName = constConfigsPath + "student_config.txt";
+    public static final String constBookConfigFilePathName = constConfigsPath + "book_config.txt";
+    public static final String constRecordConfigFilePathName = constConfigsPath + "record_config.txt";
+
+    public static final String constStudentDatabaseFilePathName = constDatabasesPath + "Student.txt";
+    public static final String constBookDatabaseFilePathName = constDatabasesPath + "Book.txt";
+    public static final String constRecordDatabaseFilePathName = constDatabasesPath + "Record.txt";
+
+    public static final String constAppIconPathName = "file:" + constImagesPath + "app_icon.png";
+    public static final String constAddImagePathName = "file:" + constImagesPath + "add.png";
+    public static final String constEditImagePathName = "file:" + constImagesPath + "edit.png";
+    public static final String constSearchImagePathName = "file:" + constImagesPath + "search.png";
+    public static final String constDeleteImagePathName = "file:" + constImagesPath + "delete.png";
+}
