edb7ca934b0d1770c4024831da9d2003eeb8d849
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-12 18:21:33 +0430

    isValid method implemented in CustomDate to check the Date Validation

--- src/main/java/model/CustomDate.java
+++ src/main/java/model/CustomDate.java
@@ -1,6 +1,9 @@
 package model;
 
 public class CustomDate {
+    private static final int minYear = 1300;
+    private static final int maxYear = 1500;
+
     private int year;
     private int month;
     private int day;
@@ -11,17 +14,13 @@ public class CustomDate {
         this.day = day;
     }
 
-    public static CustomDate createDate() {
-        return new CustomDate(-1,-1,-1);
-    }
-
     public static CustomDate intToDate(int intDate) {
         int date = intDate;
         int day = date % 100;
         date /= 100;
         int month = intDate % 100;
         date /= 100;
-        if (date > 1500 || date < 1360) {
+        if (date > maxYear || date < minYear) {
             System.out.println("Bad Int Date!");
         }
         return new CustomDate(date, month, day);
@@ -29,6 +28,19 @@ public class CustomDate {
 
     public int toInt() {
         return this.year * 10000 + this.month * 100 + this.day;
+    }
+
+    public boolean isValid() {
+        if (this.year < minYear || this.year > maxYear) {
+            return false;
+        }
+        if (this.month < 1 || this.month > 12) {
+            return false;
+        }
+        if (this.day < 1 || this.day > 31) {
+            return false;
+        }
+        return true;
     }
 
     @Override
