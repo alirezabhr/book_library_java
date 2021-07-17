d13c094097ae30421499d19f6ef1aae182660960
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-17 13:28:12 +0430

    toggle groups added in BaseSettingForm attributes

--- src/main/java/views/settings_view/BaseSettingForm.java
+++ src/main/java/views/settings_view/BaseSettingForm.java
@@ -1,9 +1,12 @@
 package views.settings_view;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Separator;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
@@ -11,12 +14,19 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import constant.MyConst;
+import views.widgets.ButtonCreator;
 import views.widgets.CustomTextField;
+import views.widgets.ImageButton;
+import views.widgets.TitleLabel;
 
 public abstract class BaseSettingForm{
+    protected String formTitle;
+    protected CustomTextField recordSizeField;
+    protected ToggleGroup recordModeGroup;
+    protected ToggleGroup stringModeGroup;
 
     // abstract methods
-    public abstract VBox getForm();
+    protected abstract HBox getMainRow();
 
     // methods
     public void show() {
@@ -34,6 +44,24 @@ public abstract class BaseSettingForm{
         VBox root = this.getForm();
         return new Scene(root, 700, 450);
     }
+    protected VBox getForm() {
+        TitleLabel titleLabel = new TitleLabel(this.formTitle);
+
+        HBox topRow = this.getTopRow();
+        HBox mainRow = this.getMainRow();
+
+        Separator separator1 = new Separator(Orientation.HORIZONTAL);
+        Separator separator2 = new Separator(Orientation.HORIZONTAL);
+
+        ImageButton saveButton = ButtonCreator.getSaveButton();
+        HBox saveRow = new HBox(saveButton);
+        saveRow.setPadding(new Insets(25));
+        saveRow.setAlignment(Pos.BASELINE_CENTER);
+
+        VBox form = new VBox(titleLabel, topRow, separator1, mainRow, separator2, saveRow);
+        form.setAlignment(Pos.TOP_CENTER);
+        return form;
+    }
     protected HBox getTopRow() {
         VBox recordModeCol = this.getRecordModeCol();
         VBox stringModeCol = this.getStringModeCol();
@@ -50,32 +78,32 @@ public abstract class BaseSettingForm{
     }
     protected VBox getRecordModeCol() {
         Label recordModeLabel = new Label("Record Mode: ");
-        ToggleGroup recordRadioGroup = new ToggleGroup();
+        this.recordModeGroup = new ToggleGroup();
 
         RadioButton radioButton1 = new RadioButton("Fix");
         RadioButton radioButton2 = new RadioButton("Dynamic");
 
-        radioButton1.setToggleGroup(recordRadioGroup);
-        radioButton2.setToggleGroup(recordRadioGroup);
+        radioButton1.setToggleGroup(this.recordModeGroup);
+        radioButton2.setToggleGroup(this.recordModeGroup);
 
         return new VBox(recordModeLabel, radioButton1, radioButton2);
     }
     protected VBox getStringModeCol() {
         Label stringModeLabel = new Label("String Mode: ");
-        ToggleGroup stringRadioGroup = new ToggleGroup();
+        this.stringModeGroup = new ToggleGroup();
 
         RadioButton radioButton1 = new RadioButton("Fix");
         RadioButton radioButton2 = new RadioButton("Dynamic");
 
-        radioButton1.setToggleGroup(stringRadioGroup);
-        radioButton2.setToggleGroup(stringRadioGroup);
+        radioButton1.setToggleGroup(this.stringModeGroup);
+        radioButton2.setToggleGroup(this.stringModeGroup);
 
         return new VBox(stringModeLabel, radioButton1, radioButton2);
     }
     protected VBox getRecordSizeCol() {
         Label recordSizeLabel = new Label("Record Size: ");
-        CustomTextField recordSizeField = new CustomTextField("record size...");
+        this.recordSizeField = new CustomTextField("record size...");
 
-        return new VBox(recordSizeLabel, recordSizeField);
+        return new VBox(recordSizeLabel, this.recordSizeField);
     }
 }
