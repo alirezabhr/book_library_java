e81cec3a630f37272d073c870d3a5c7d1add7d05
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-07-17 13:12:31 +0430

    StudentSettingForm created and implemented methods

--- src/main/java/views/settings_view/StudentSettingForm.java
+++ src/main/java/views/settings_view/StudentSettingForm.java
@@ -1,18 +1,45 @@
 package views.settings_view;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import views.widgets.ButtonCreator;
-import views.widgets.ImageButton;
+import views.widgets.CustomTextField;
 
 public class StudentSettingForm extends BaseSettingForm {
+    protected CustomTextField nameSizeField;
+    protected CustomTextField lastNameSizeField;
+
+    // constructors
+    public StudentSettingForm() {
+        this.formTitle = "<Student Config Settings>";
+    }
 
     // methods
-    public VBox getForm() {
-        HBox topRow = this.getTopRow();
+    protected HBox getMainRow() {
+        VBox nameCol = this.getNameCol();
+        VBox lastNameCol = this.getLastNameCol();
 
-        ImageButton saveButton = ButtonCreator.getSaveButton();
-        return new VBox(topRow, saveButton);
+        HBox mainRow = new HBox(nameCol, lastNameCol);
+        mainRow.setSpacing(80);
+        mainRow.setPadding(new Insets(20));
+        return mainRow;
+    }
+    private VBox getNameCol() {
+        Label nameLabel = new Label("Student Name Size: ");
+        this.nameSizeField = new CustomTextField("name size...");
+
+        VBox nameCol = new VBox(nameLabel, this.nameSizeField);
+        nameCol.setSpacing(5);
+        return nameCol;
+    }
+    private VBox getLastNameCol() {
+        Label lastNameLabel = new Label("Student Last Name Size: ");
+        this.lastNameSizeField = new CustomTextField("last name size...");
+
+        VBox lastNameCol = new VBox(lastNameLabel, this.lastNameSizeField);
+        lastNameCol.setSpacing(5);
+        return lastNameCol;
     }
 }
